0a79ae76f28bb1a8ae98d4c56a1bc5caf63ec321
Matheus Henrique <mathhas.dev@gmail.com>
2025-11-09 12:50:21 -0300

    initial commit

---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { countries } from './contries'
+
+  let selectedCountry = ''
+  let cities = []
+
+  const countryCitiesMap: Record<string, string[]> = {
+    Brazil: ['São Paulo', 'Rio de Janeiro', 'Brasília'],
+    Canada: ['Toronto', 'Vancouver', 'Montreal'],
+    Denmark: ['Copenhagen', 'Aarhus', 'Odense'],
+    Egypt: ['Cairo', 'Alexandria', 'Giza'],
+    France: ['Paris', 'Lyon', 'Marseille'],
+    Germany: ['Berlin', 'Munich', 'Frankfurt'],
+  }
+</script>
+
+<svelte:head>
+  <title>Demo</title>
+  <meta name="description" content="A Wordle clone written in SvelteKit" />
+</svelte:head>
+
+<h1>See countries cities</h1>
+
+<ul>
+  {#each countries as country}
+    <li>{country}</li>
+    <ul>
+      {#each countryCitiesMap[country] as city}
+        <li>{city}</li>
+      {/each}
+    </ul>
+  {/each}
+</ul>
---
+++ src/routes/demo/contries.ts
@@ -0,0 +1,8 @@
+export const countries = [
+  "Brazil",
+  "Canada",
+  "Denmark",
+  "Egypt",
+  "France",
+  "Germany",
+];
